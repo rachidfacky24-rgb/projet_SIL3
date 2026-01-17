d4fbcf24ae4562ce6dc73864849764d927d82fa5
rachidfacky24-rgb <rachidfacky24@gmail.com>
2026-01-17 23:35:03 +0100

    Suppression de toutes les modifications liées à PythonAnywhere

--- Assignation/assignation_project/settings.py
+++ Assignation/assignation_project/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-change-this-in-production-!@#$%^&*()'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
